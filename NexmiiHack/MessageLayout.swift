26a5e9cd60c8434859cabab5630f6a85219415f0
Or Ron <or.ron10@gmail.com>
2016-10-28 02:47:56 +0300

    added bubble view

--- NexmiiHack/MessageLayout.swift
+++ NexmiiHack/MessageLayout.swift
@@ -8,13 +8,51 @@
 
 import LayoutKit
 
+
+class BubbleViewSend: UIView {
+    
+    override func draw(_ rect: CGRect) {
+        let path = UIBezierPath(roundedRect: rect.insetBy(dx: 5, dy: 5), cornerRadius: 10)
+        UIColor.red.setFill()
+        path.fill()
+        
+        let triPath = UIBezierPath()
+        path.move(to: CGPoint(x:20, y: rect.size.height-5))
+        path.addLine(to: CGPoint(x: 20, y: rect.size.height))
+        path.addLine(to: CGPoint(x: 20+3+3, y: rect.size.height-5))
+        path.addLine(to: CGPoint(x:20, y: rect.size.height-5))
+        UIColor.red.setFill()
+        path.fill()
+    }
+    
+}
+
+class BubbleViewRecv: UIView {
+    
+    override func draw(_ rect: CGRect) {
+        let path = UIBezierPath(roundedRect: rect.insetBy(dx: 5, dy: 5), cornerRadius: 10)
+        UIColor.red.setFill()
+        path.fill()
+        
+        let triPath = UIBezierPath()
+        path.move(to: CGPoint(x:rect.size.width-20, y: rect.size.height-5))
+        path.addLine(to: CGPoint(x: rect.size.width-20, y: rect.size.height))
+        path.addLine(to: CGPoint(x: rect.size.width-20-3-3, y: rect.size.height-5))
+        path.addLine(to: CGPoint(x:rect.size.width-20, y: rect.size.height-5))
+        UIColor.red.setFill()
+        path.fill()
+    }
+    
+}
+
+
 extension String {
     func getLayout() -> Layout {
         return
-            InsetLayout<UIView>(insets: UIEdgeInsets(top: 15, left: 15, bottom: 15, right: 15),
+            InsetLayout<BubbleViewRecv>(insets: UIEdgeInsets(top: 15, left: 15, bottom: 15, right: 15),
                                 sublayout: LabelLayout<UILabel>(text:self)) {view in
         
-                                    //view.backgroundColor = .red
+                                    view.backgroundColor = .clear
         }
     }
 }
