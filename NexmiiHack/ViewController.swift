282dcb38eb8b29a91661033d1df0e0204b81c4ca
Vladimir Kofman <vladimir.kofman@intel.com>
2016-10-27 23:27:51 +0300

    Initial messaging with LayoutKit

--- NexmiiHack/ViewController.swift
+++ NexmiiHack/ViewController.swift
@@ -7,19 +7,79 @@
 //
 
 import UIKit
+import LayoutKit
+import Dwifft
 
 class ViewController: UIViewController {
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var reloadableAdapter: ReloadableViewLayoutAdapter!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.reloadableAdapter = ReloadableViewLayoutAdapter(reloadableView: self.collectionView)
+        self.collectionView.delegate = self.reloadableAdapter
+        self.collectionView.dataSource = self.reloadableAdapter
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+            self.messages = ["Hello There"]
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+            self.messages.append("What's")
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 6) {
+            self.messages.append("UP?")
+        }
     }
+    
+    var messages = [String]() {
+        willSet {
+            let diff = self.messages.diff(newValue)
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+            let batchUpdates: BatchUpdates = {
+                var updates = BatchUpdates()
+                
+                updates.insertItems = diff.insertions.map({ IndexPath(row: $0.idx, section: 0) })
+                
+                return updates
+            }()
+            
+            if batchUpdates.insertItems.count == 0 {
+                return
+            }
+            
+            let layouts = newValue.map{ $0.getLayout() }
+            
+            reloadableAdapter.reload(width:self.collectionView.frame.width, synchronous: false, batchUpdates: batchUpdates, layoutProvider: {
+                return [Section(header: nil, items: layouts, footer: nil)]
+            }) {
+                
+//                if !newValue.isEmpty {
+//                    self.hideLoading()
+//                }
+//                
+//                self.scrollAndBounceTheLatestIntentIdIfNeeded(self.latestIntentId)
+//                self.latestIntentId = nil
+//                
+//                self.updatePullPager(NSDate(long: stateDate))
+//                
+//                delay(0.5) {
+//                    self.reloadableAdapter.updateTitle(self.timeLineCollectionVC)
+//                }
+            }
+            
+        }
     }
+}
 
-
+extension String {
+    func getLayout() -> Layout {
+        return
+            InsetLayout<UIView>(insets: UIEdgeInsets(top: 15, left: 15, bottom: 15, right: 15),
+                            sublayout: LabelLayout<UILabel>(text:self))
+    }
 }
 
